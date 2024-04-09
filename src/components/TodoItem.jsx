db6e2a417c3ccd07cd75304ca21c5c063597d924
LeoMejia <leonardomg27@hotmail.com>
2024-04-09 13:32:40 -0500

    FinDelProyecto

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,13 @@
 import IconCross from "./icons/IconCross";
 import PropTypes from "prop-types";
 import IconCheck from "./icons/IconCheck";
+import React from "react";
 
-const TodoItem = ({ todo, removerTodo, updateTodo }) => {
+const TodoItem = React.forwardRef(({ todo, removerTodo, updateTodo, ...props}, ref) => {
     const { id, title, completed } = todo;
 
     return (
-        <article className="flex gap-4 border-b border-b-gray-400">
+        <article {...props} ref={ref} className="flex gap-4 border-b border-b-gray-400">
             <button
                 className={`h-5 w-5 flex-none rounded-full border-2 ${
                     completed
@@ -17,7 +18,9 @@ const TodoItem = ({ todo, removerTodo, updateTodo }) => {
             >
                 {completed && <IconCheck />}
             </button>
-            <p className={`grow dark:text-gray-400 text-gray-600 ${completed && "line-through"}`}>
+            <p
+                className={`grow text-gray-600 dark:text-gray-400 ${completed && "line-through"}`}
+            >
                 {title}
             </p>
             <button className="flex-none" onClick={() => removerTodo(id)}>
@@ -25,7 +28,9 @@ const TodoItem = ({ todo, removerTodo, updateTodo }) => {
             </button>
         </article>
     );
-};
+});
+TodoItem.displayName = "TodoItem";
+
 TodoItem.propTypes = {
     todo: PropTypes.object,
     removerTodo: PropTypes.func,
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,39 @@
+import { Droppable, Draggable } from "@hello-pangea/dnd";
 import TodoItem from "./TodoItem";
 import PropTypes from "prop-types";
 
 const TodoList = ({ todos, removerTodo, updateTodo }) => {
     return (
-        <div className="mt-8 overflow-hidden rounded-t-md border-b-gray-400 bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4">
-            {todos.map((todo) => (
-                <TodoItem
-                    key={todo.id}
-                    todo={todo}
-                    removerTodo={removerTodo}
-                    updateTodo={updateTodo}
-                />
-            ))}
-        </div>
+        <Droppable droppableId="todos">
+            {(dropableProvider) => (
+                <div
+                    ref={dropableProvider.innerRef}
+                    {...dropableProvider.droppableProps}
+                    className="mt-8 overflow-hidden rounded-t-md border-b-gray-400 bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+                >
+                    {todos.map((todo, index) => (
+                        <Draggable
+                            key={todo.id}
+                            index={index}
+                            draggableId={`${todo.id}`}
+                        >
+                            {(DraggableProvider) => (
+                                <TodoItem
+                                    todo={todo}
+                                    removerTodo={removerTodo}
+                                    updateTodo={updateTodo}
+                                    ref={DraggableProvider.innerRef}
+                                    {...DraggableProvider.dragHandleProps}
+                                    {...DraggableProvider.draggableProps}
+                                />
+                            )}
+                        </Draggable>
+                    ))}
+
+                    {dropableProvider.placeholder}
+                </div>
+            )}
+        </Droppable>
     );
 };
 
